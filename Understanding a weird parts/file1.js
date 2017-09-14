4b06a11909bc77973ce747e0750f3f99c066d186
gregtr <t.grzegorz90@gmail.com>
2017-09-14 12:21:08 +0100

    Weird stuff tutorial - first commit

---
+++ Understanding a weird parts/file1.js
@@ -0,0 +1,13 @@
+/* Big words and javascript 
+
+    Big word: definition - thy're not scary as they sound
+
+
+/* 
+Understanding, frameworks, and the WEIRD parts 
+    !!! We need to understand how javascript work's - fundamental knowledge
+    will come in handy when we encounter something bizzare 
+        - plenty of "AHAAAA" moments !!!
+        
+        jQuery, AngularJS - framework's of javascript - just javascript code developed by smart developers - but most of javascript developer's won't understand those frameworks - cus they don't understand javascript enough
+*/
